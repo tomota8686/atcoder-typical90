3505475b24272eb648030540bc9dc0abb18b13ec
Tomotaka Iwakoshi <tomota8686@gmail.com>
2022-10-06 18:44:34 +0900

    累積和をとる(失敗)

--- 010.cpp
+++ 010.cpp
@@ -2,28 +2,32 @@
 using namespace std;
 
 int  n, q, l, r;
-int one, two;
+int tmp;
+int tm_c, tm_p;
 
 int main() {
+    //入力
     cin >> n;
-
     int c[n+1], p[n+1];
-
+    int tm_c = 0;
+    int tm_p = 0;
     for(int i=1; i<=n; i++) {
-        cin >> c[i] >> p[i];
+        tmp = 0;
+        cin >> tmp;
+        tm_c += tmp;
+        cin >> tmp;
+        tm_p += tmp;
+        c[i] = tm_c;
+        p[i] = tm_p;
     }
+
     cin >> q;
     int l[q+1], r[q+1];
     for(int i=1; i<=q; i++) cin >> l[i] >> r[i];
 
+        //処理
     for(int i=1; i<=q; i++) {
-        one = 0;
-        two = 0;
-        for(int j=l[i]; j<=r[i]; j++) {
-            if(c[j]==1) one += p[j];
-            else if(c[j]==2) two += p[j];
-        }
-        cout << one << " " << two << endl;
+        
     }
 
     return 0;
