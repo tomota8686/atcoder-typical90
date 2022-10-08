fc1d1d8d965d216d507c4b7cd3136bbb4c56e078
Tomotaka Iwakoshi <tomota8686@gmail.com>
2022-10-08 20:50:18 +0900

    033, 055 init

---
+++ 055.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+long long n, p, q;
+
+int main() {
+    cin >> n >> p >> q;
+    long long a[n+1];
+    for (int i=1; i<n+1; i++) {
+        cin >> a[i];
+    }
+
+    
+}
